3a359e73f4fd13c5bd2397d4dca8d688644cd268
Evandro Rodrigues Coelho <evandrorodcoelho@gmail.com>
2025-02-26 10:05:11 -0300

    chore: Add new datas

--- src/app/mock/philosophiesData.ts
+++ src/app/mock/philosophiesData.ts
@@ -46,4 +46,45 @@ export const philosophiesData: Philosophies = [
     ],
     quote: 'Às coisas em si mesmas!',
   },
+  {
+    name: 'Ceticismo',
+    type: 'epistemology',
+    description:
+      'Uma abordagem filosófica que questiona a possibilidade do conhecimento absoluto.',
+    key_figures: ['Pirro de Élis', 'Sexto Empírico'],
+    quote: 'A única coisa que sei é que nada sei.',
+  },
+  {
+    name: 'Materialismo Dialético',
+    type: 'metaPhysics',
+    description:
+      'Uma abordagem filosófica e metodológica desenvolvida por Karl Marx e Friedrich Engels, baseada no materialismo e na dialética.',
+    key_figures: ['Karl Marx', 'Friedrich Engels'],
+    quote:
+      'A história de todas as sociedades até hoje tem sido a história das lutas de classes.',
+  },
+  {
+    name: 'Idealismo',
+    type: 'metaPhysics',
+    description:
+      'Uma corrente filosófica que afirma que a realidade é fundamentalmente mental ou imaterial.',
+    key_figures: ['Platão', 'Immanuel Kant', 'Georg Wilhelm Friedrich Hegel'],
+    quote: 'O mundo sensível é apenas uma sombra do mundo das ideias.',
+  },
+  {
+    name: 'Pragmatismo',
+    type: 'epistemology',
+    description:
+      'Uma filosofia que enfatiza a experiência prática e os resultados como fundamentos do conhecimento.',
+    key_figures: ['Charles Sanders Peirce', 'William James', 'John Dewey'],
+    quote: 'A verdade é aquilo que funciona.',
+  },
+  {
+    name: 'Contractualismo',
+    type: 'ethics',
+    description:
+      'Uma teoria ética e política que defende que a moralidade e a legitimidade política derivam de um contrato social.',
+    key_figures: ['Thomas Hobbes', 'John Locke', 'Jean-Jacques Rousseau'],
+    quote: 'O homem nasce livre, mas em toda parte encontra-se acorrentado.',
+  },
 ];
